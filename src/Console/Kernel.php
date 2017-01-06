3383b71f7d61d68dee45fec9f7444d1e54232740
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2017-01-06 01:02:48 +0100

    Added basic laravel classes

---
+++ src/Console/Kernel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Glagol\Bridge\Laravel\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Register the Closure based commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        require base_path('routes/console.php');
+    }
+}
