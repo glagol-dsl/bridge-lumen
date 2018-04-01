d23139933ab3d7d1aef810ef59824d472cb77890
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2018-04-01 18:33:33 +0200

    Added DateTime value object factory (doctrine type)

--- src/Value/DateTime.php
+++ src/Value/DateTime.php
@@ -1,8 +1,23 @@
 <?php
 namespace Glagol\Bridge\Lumen\Value;
 
+use DateTimeZone;
+
 class DateTime extends \DateTimeImmutable implements \JsonSerializable
 {
+    public static function createFromFormat($format, $time, DateTimeZone $timezone = null)
+    {
+        $realDt = new static();
+        $parentDt = parent::createFromFormat($format, $time, $timezone);
+
+        if (!$parentDt) {
+            return false;
+        }
+
+        $realDt->setTimestamp($parentDt->getTimestamp());
+        return $realDt;
+    }
+
     public function jsonSerialize()
     {
         return $this->format('c');
---
+++ src/Value/DateTimeType.php
@@ -0,0 +1,67 @@
+<?php
+namespace Glagol\Bridge\Lumen\Value;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeTzType;
+
+class DateTimeType extends DateTimeTzType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'glagol_datetime_datetime';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value) {
+            return $value;
+        }
+
+        if ($value instanceof DateTime) {
+            return $value->format($platform->getDateTimeTzFormatString());
+        }
+
+        throw ConversionException::conversionFailedInvalidType(
+            $value,
+            $this->getName(),
+            ['null', DateTime::class]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null || $value instanceof DateTime) {
+            return $value;
+        }
+
+        $dateTime = DateTime::createFromFormat($platform->getDateTimeTzFormatString(), $value);
+
+        if (! $dateTime) {
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeTzFormatString()
+            );
+        }
+
+        return $dateTime;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform)
+    {
+        return true;
+    }
+}
